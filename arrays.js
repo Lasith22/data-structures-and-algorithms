09f29ccb33e9eb907fbbfafa1397055bfd8b7030
lasith herath <lasithherath00@gmail.com>
2024-08-03 07:44:21 +0530

    Learn about arrays and array methods

---
+++ arrays.js
@@ -0,0 +1,13 @@
+let fruits = ['Apple', 'orange', 'banana'];
+
+fruits.push('Kesel');
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+
+//enhanced for loop
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
