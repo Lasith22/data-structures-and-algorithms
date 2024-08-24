7d65e18c6975ebe1c5a845a8c55a03b349d1a196
lasith herath <lasithherath00@gmail.com>
2024-08-24 17:35:10 +0530

    Recap spread op

---
+++ ALL Js/spreadOp.js
@@ -0,0 +1,14 @@
+let numbers = [1, 2, 3, 4, 5];
+
+let maximum = Math.max(...numbers);
+let minimum = Math.min(...numbers);
+
+let userName = 'Bro code';
+
+let letters = [...userName].join('-');
+// console.log(maximum);
+// console.log(minimum);
+console.log(letters);
+
+console.log(...numbers);
+console.log(numbers);
