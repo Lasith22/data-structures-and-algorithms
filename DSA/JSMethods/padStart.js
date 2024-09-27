495e0cfd05be2c2a1ed31a4066f265f6356fc1ea
lasith herath <lasithherath00@gmail.com>
2024-09-27 09:27:41 +0530

    Learn padStart

---
+++ DSA/JSMethods/padStart.js
@@ -0,0 +1,9 @@
+// let str = '5';
+// let paddedStr = str.padStart(4);
+// console.log(paddedStr);
+// let num = '123';
+// let paddedNum = num.padStart(6, '0');
+// console.log(paddedNum);
+let word = 'hello';
+let paddedWord = word.padEnd(10, '*');
+console.log(paddedWord);
