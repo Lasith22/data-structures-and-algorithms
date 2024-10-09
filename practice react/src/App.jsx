7500a786cd2b5a00c3c1373c9907b70386a51877
lasith herath <lasithherath00@gmail.com>
2024-10-09 07:47:40 +0530

    Recam custom hooks

--- practice react/src/App.jsx
+++ practice react/src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-import Hello from "../src/components/jsx/Hello";
-import LearnProps from "./components/props/LearnProps";
-import DisplayCount from "./components/props/DisplayCount";
-import UseState from "./components/hooks/UseState";
-import TestUseEffect from "./components/hooks/UseEffect";
-import UseRef from "./components/hooks/UseRef";
+import { useState } from 'react';
+import Hello from '../src/components/jsx/Hello';
+import LearnProps from './components/props/LearnProps';
+import DisplayCount from './components/props/DisplayCount';
+import UseState from './components/hooks/UseState';
+import TestUseEffect from './components/hooks/UseEffect';
+import UseRef from './components/hooks/UseRef';
 function App() {
   const [count, setCount] = useState(1);
 
   return (
     <>
-      {/* <Hello /> */}
+      <Hello />
       {/* <LearnProps name="lasithsss" />
       <DisplayCount count={count} setCount={setCount} /> */}
       {/* <UseState /> */}
       {/* <TestUseEffect /> */}
-      <UseRef />
+      {/* <UseRef /> */}
     </>
   );
 }
---
+++ practice react/src/components/jsx/Hello2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Hello2 = () => {
+  return (
+    <>
+      <h2>Hello Pakaua</h2>
+    </>
+  );
+};
+export default Hello2;
