a631e015ed20bd46ea579c477af1c943d1f7c186
lasith herath <lasithherath00@gmail.com>
2024-10-17 13:14:52 +0530

    Started redux project

---
+++ practice react/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <>
+      <h1>Profile</h1>
+      <p>Name:</p>
+      <p>Age:</p>
+      <p>Email:</p>
+    </>
+  );
+};
+
+export default Profile;
