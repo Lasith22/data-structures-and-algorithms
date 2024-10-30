d494bedb07c3092f7668c96b0c2ef4db11115148
lasith herath <lasithherath00@gmail.com>
2024-10-30 07:18:20 +0530

    Big O

--- FULL_DSA/index.js
+++ FULL_DSA/index.js
@@ -1,11 +1,23 @@
-const studentDatabase = ['jordan', 'erick', 'john', 'michel'];
+// const studentDatabase = ['jordan', 'erick', 'john', 'michel'];
 
-const findStudent = (allStudents, studentName) => {
-  for (let i = 0; i < allStudents.length; i++) {
-    if (allStudents[i] === studentName) {
-      console.log('Found', studentName);
+// const findStudent = (allStudents, studentName) => {
+//   for (let i = 0; i < allStudents.length; i++) {
+//     if (allStudents[i] === studentName) {
+//       console.log('Found', studentName);
+//     }
+//   }
+// };
+
+// findStudent(studentDatabase, 'erickf');
+
+const groceries = ['milk', 'bread', 'eggs', 'flour', 'choose', 'sugar'];
+
+const searchForItem = (item) => {
+  for (let i = 0; i < groceries.length; i++) {
+    if (groceries[i] === item) {
+      console.log('Found', item);
     }
   }
 };
 
-findStudent(studentDatabase, 'erickf');
+searchForItem('milk');
